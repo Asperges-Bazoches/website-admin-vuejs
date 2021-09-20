14fbc626726540c67ddef82e79424a518faf65f1
Martin M <martin.masson@datastorm.fr>
2021-09-20 19:54:23 +0200

    WIP : To be erased soon

--- champramard-admin/src/components/ProductActivation.vue
+++ champramard-admin/src/components/ProductActivation.vue
@@ -23,7 +23,7 @@
     </v-list-item>
 
     <v-card-actions>
-      <v-btn text to="/settings">{{this.btnMsg}}</v-btn>
+      <v-btn text @click="setAvailability()">{{this.btnMsg}}</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -46,47 +46,79 @@
       btnMsg: 'Paramétrer',
       msg : 'Erreur : Impossible de lire l\'état actuel',
     }),
+
     mounted () {
-
-      const user = localStorage.getItem('user');
-      axios
-      .get('/v2/public/settings.php',
-        {headers: {
-          'Access-Control-Allow-Origin': "*",
-          'Authorization': 'Basic ' + user,
-          'Content-Type': 'application/json',
-         }})
-      .then(response => (this.response=response.data))
-      .catch(()=>(this.response=null))
-
+      this.getAvailibility();
     },
+
     watch: {
       response: function(val){
-
         var availability=null;
-
         for(var k in val) {
           if(val[k]['STR_KEY']===this.prodId){
             availability = val[k]['STR_VALUE'];
           }
         }
-
         if(availability=="true"){
+          this.availability = true;
           this.btnMsg='Rendre indisponible';
           this.msg='Ce produit est actuellement commandable depuis le site.';
           this.header='PRODUIT DISPONIBLE';
         }else if(availability=="false"){
+          this.availability = false;
           this.btnMsg='Rendre disponible';
           this.msg='Ce produit est actuellement incommandable depuis le site.';
           this.header='PRODUIT INDISPONIBLE';
         }else{
+          this.availability = null;
           this.btnMsg='Paramétrer';
           this.msg='Impossible de lire le statut actuel';
           this.header='ERREUR';
         }
-
         this.loading = false;
       }
-    }
+    },
+
+    methods: {
+      getAvailability(){
+        const user = localStorage.getItem('user');
+        axios
+        .get('/v2/public/settings.php',
+          {headers: {
+            'Access-Control-Allow-Origin': "*",
+            'Authorization': 'Basic ' + user,
+            'Content-Type': 'application/json',
+           }})
+        .then(response => {
+          this.response=response.data;
+          this.getAvailibility();
+        })
+        .catch(()=>(this.response=null))
+      },
+      setAvailability() {
+        var bodyFormData = new FormData();
+        if(this.availability){
+          bodyFormData.append('this.prodId', 'false');
+        } else {
+          bodyFormData.append('this.prodId', 'true');
+        }
+        const requestOptions = {
+            data: bodyFormData,
+            headers: { 'Content-Type': 'application/json',
+                       "Access-Control-Allow-Origin": "*",
+                       'Authorization': 'Basic ' + localStorage.getItem('user'),
+                     }
+        };
+
+        axios
+        .post('v2/private/settings.php', requestOptions)
+        .then((response) => {
+          console.log(response.data.res);
+        })
+        .catch(() => {
+          console.log("Error while changing availability");
+        })
+      }
+    },
   }
 </script>
