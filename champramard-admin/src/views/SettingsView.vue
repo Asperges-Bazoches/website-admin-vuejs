6949f44ed686e34a54cc2b04ddba5a0339cfb4cf
Martin M <martin.masson@datastorm.fr>
2021-10-03 15:35:05 +0200

    FEAT: Get/Set more settings

--- champramard-admin/src/views/SettingsView.vue
+++ champramard-admin/src/views/SettingsView.vue
@@ -23,7 +23,10 @@
           />
         </v-col>
       </v-row>
-      <br/>
+
+      <br/><br/><br/>
+
+      <h2>Plus de paramétrages</h2>
 
       <v-text-field
         v-model="websiteTitle"
@@ -34,7 +37,6 @@
         v-model="websiteSubtitle"
         label="Sous-titre du site"
       ></v-text-field>
-
 
       <v-row>
         <v-col cols="4">
@@ -51,11 +53,23 @@
         </v-col>
       </v-row>
 
+      <br/>
+
+      <v-btn
+        large
+        block
+        color='primary'
+        @click="setSettings()">
+        Mettre à jour
+      </v-btn>
+
+      <br/><br/><br/><br/>
+
     </div>
   </template>
 
   <script>
-  // @ is an alias to /src
+  import axios from 'axios';
   import ProductActivation from '@/components/ProductActivation.vue'
 
   export default {
@@ -68,7 +82,76 @@
       websiteSubtitle: '',
       emailTarget: '',
       allowEmail: true,
-    })
+      sateSettings: {},
+      errMsg: '',
+    }),
+
+    methods: {
+      getSettings: function () {
+        this.loading = true;
+        const user = localStorage.getItem('user');
+        axios
+        .get('/v2/public/settings.php',
+          {headers: {
+            'Access-Control-Allow-Origin': "*",
+            'Authorization': 'Basic ' + user,
+            'Content-Type': 'application/json',
+           }})
+        .then(response => {
+          let val = response.data;
+          for(var k in val) {
+            this.sateSettings[val[k]['STR_KEY']] = val[k]['STR_VALUE'];
+          }})
+        .then(()=>{
+          if('website_title' in this.sateSettings){
+            this.websiteTitle = this.sateSettings['website_title'];
+          }
+          if('website_subtitle' in this.sateSettings){
+            this.websiteSubtitle = this.sateSettings['website_subtitle'];
+          }
+          if('email_target' in this.sateSettings){
+            this.emailTarget = this.sateSettings['email_target'];
+          }
+          if('allow_email' in this.sateSettings){
+            this.allowEmail = this.sateSettings['allow_email'];
+          }
+        })
+        .catch(()=> {
+          this.errMsg='Impossible de lire le paramétrage actuel';
+        })
+      },
+
+      setSettings: function() {
+        var bodyFormData = new FormData();
+        if(this.websiteTitle != ''){
+          bodyFormData.append('website_title', this.websiteTitle);
+        }
+        if(this.websiteSubtitle != ''){
+          bodyFormData.append('website_subtitle', this.websiteSubtitle);
+        }
+        if(this.emailTarget != ''){
+          bodyFormData.append('email_target', this.emailTarget);
+        }
+        bodyFormData.append('allow_email', this.allowEmail);
+
+        const requestOptions = {
+            headers: {
+                       'Access-Control-Allow-Origin': '*',
+                       'Authorization': 'Basic ' + localStorage.getItem('user'),
+                       'Content-Type': 'application/json',
+                     }
+        };
+
+        axios
+        .post('v2/private/settings.php', bodyFormData, requestOptions)
+        .then(() => (this.getAvailability()))
+        .catch(() => (console.log("Error while changing availability")));
+      },
+    },
+
+    mounted () {
+      this.getSettings();
+    },
   }
   </script>
 
